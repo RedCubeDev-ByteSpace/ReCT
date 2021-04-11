30cf907b612ea38c0a00da991f4859d343294a6b
RedCubeDev <61469501+RedCubeDev-ByteSpace@users.noreply.github.com>
2021-04-12 01:55:38 +0200

    messed up the net and math package lol

--- ReCT/info.cs
+++ ReCT/info.cs
@@ -6,6 +6,6 @@ namespace ReCT
 {
     public static class info
     {
-        public static string Version = "v2.2.1"; // <- DJs semicolon
+        public static string Version = "v2.2.2"; // <- DJs semicolon
     }
 }
